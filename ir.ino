7d1efe1351a4a3396a153aee10be07a6a6b796e7
Manu M B <47355913+mbmanu@users.noreply.github.com>
2020-06-01 10:39:43 +0530

    Add files via upload

---
+++ ir.ino
@@ -0,0 +1,23 @@
+int ir=10;
+int led=13;
+int x;
+void setup()
+{
+  pinMode(10,INPUT);
+  pinMode(13,OUTPUT);
+}
+void loop()
+{
+ x=digitalRead(10);
+ delay(3);
+ if(x==LOW)
+ {
+  digitalWrite(13,LOW);
+  delay(500);
+ }
+ else
+ {
+  digitalWrite(13,HIGH);
+  delay(100);
+ }
+}
